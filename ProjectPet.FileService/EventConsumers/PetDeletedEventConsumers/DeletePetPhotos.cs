1ac1c282f5eb8ff6e272e40b3179c2c7c76b97dd
brinersam <vladbudyachenko@mail.ru>
2025-08-16 00:49:17 +0300

    Добавлен rabbitMQ, консюмер PetDeletedEvent

---
+++ ProjectPet.FileService/EventConsumers/PetDeletedEventConsumers/DeletePetPhotos.cs
@@ -0,0 +1,45 @@
+﻿using DEVShared;
+using MassTransit;
+using ProjectPet.FileService.Infrastructure.AmazonS3;
+
+namespace ProjectPet.FileService.EventConsumers.PetDeletedEventConsumers;
+
+public class DeletePetPhotos : IConsumer<PetDeletedEvent>
+{
+    private readonly IS3Provider _s3Provider;
+    private readonly ILogger<DeletePetPhotos> _logger;
+
+    public DeletePetPhotos(
+        IS3Provider s3Provider,
+        ILogger<DeletePetPhotos> logger
+        )
+    {
+        _s3Provider = s3Provider;
+        _logger = logger;
+    }
+
+    public async Task Consume(ConsumeContext<PetDeletedEvent> context)
+    {
+        var petId = context.Message.PetId;
+        var photosToDelete = context.Message.FileLocations;
+
+        await Task.WhenAll(
+                photosToDelete.Select(async x =>
+                {
+                    var deleteResult = await _s3Provider.DeleteFileAsync(x, CancellationToken.None);
+                    if (deleteResult.IsFailure)
+                    {
+                        _logger.LogWarning(
+                            "Failed to delete photo (id: {photoId}) that was owned by now deleted pet (id: {petId}): {error}",
+                            x.FileId,
+                            petId,
+                            deleteResult.Error.Message
+                        );
+                    }
+
+                    return deleteResult;
+                }
+                )
+        );
+    }
+}
